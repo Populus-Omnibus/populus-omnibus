eeeb93ff2a56fd823658dfeacd34122aaaef6de2
voroscsoki <69348889+voroscsoki@users.noreply.github.com>
2022-01-13 02:42:08 +0100

    Added test ping command

---
+++ Discord/Misc.cs
@@ -0,0 +1,25 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Populus.Commands
+{
+    public class MiscSlash : ApplicationCommandModule
+    {
+        [SlashCommand("ping", "Returns the current latency of the Discord bot.")]
+        public async Task SlashPing(InteractionContext ctx)
+        {
+            var responsebuilder = new DiscordInteractionResponseBuilder()
+            {
+                Content = $"{Global.Discord.Ping} ms"
+            };
+            await ctx.CreateResponseAsync(responsebuilder);
+        }
+    }
+}
